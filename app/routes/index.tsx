819555b5f3c9198f2167d374092d1b9552e5795f
bangsyir <bangsyir@gmail.com>
2022-02-10 19:41:15 +0800

    update some style

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "remix";
 
 export default function Index() {
   return (
-    <div style={{display:"flex", flexDirection:"column" ,justifyContent:"center", alignItems:"center", paddingTop:"25%"}}>
+    <div style={{display:"flex", flexDirection:"column" ,justifyContent:"center", alignItems:"center", paddingTop:"10%"}}>
       <h1>
         welcome To Our Blog
       </h1>
