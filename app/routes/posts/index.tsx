ecb17b4522adfc69681017f7fdf7d118bf634879
bangsyir <bangsyir@gmail.com>
2022-02-10 19:39:43 +0800

    add ErrorCoundary and CatchBoundary

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,11 +1,14 @@
 import BlogCard from "../../components/BlogCard"
 // import data from "../../../data"
-import { json, Link, LoaderFunction, useLoaderData } from "remix";
+import { json, Link, LoaderFunction, useCatch, useLoaderData } from "remix";
 import {db} from "../../utils/db.server"
 import type {Post} from "@prisma/client"
 
 export const loader:LoaderFunction = async() => {
   const data = await db.post.findMany({orderBy: {createdAt: 'desc'}})
+  if(!data) {
+    throw new Response("No posts found", {status:404})
+  }
   return json(data)
 }
 
@@ -23,4 +26,25 @@ export default function Index() {
       ))}
     </div>
   );
+}
+export function ErrorBoundary() {
+  return (
+    <div className="errorContainer">
+      <p>
+      {` There was an error loading posts, Sorry.`}
+      </p>
+      <Link to={"/"}>Go back to Home</Link>
+    </div>
+  )
+}
+
+export function CatchBoundary() {
+  const caught = useCatch()
+  if(caught.status === 404) {
+    return (
+      <div className="error-container">
+        There are no posts to display.
+      </div>
+    )
+  }
 }
